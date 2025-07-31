72a824665bf8c709e5a9ca94f622f62e52c03c76
tdpnhi <trandophuongnhi111@gmail.com>
2025-08-01 02:28:10 +0900

    feat (ingredients): completed create, update, delete and getAll

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { IngredientsModule } from './modules/ingredients/ingredients.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, IngredientsModule],
 })
 export class AppModule {}
